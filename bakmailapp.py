7637f58b4a592498a5bded044b0884a00803e35c
liuboss1992 <liuchuanbin1992@gmail.com>
2018-10-23 20:32:23 +0800

    更新邮件应用

--- bakmailapp.py
+++ bakmailapp.py
@@ -1,13 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-import argparse
-
-parser = argparse.ArgumentParser(description='Mail Sender')
-parser.add_argument('--sn', default='lcb592', type=str, help='name of sender account, must be ustc mail')
-parser.add_argument('--pw', default='123456', type=str, help='password of your account')
-parser.add_argument('--ra', default='774054270@qq.com', type=str, help='address of the receiver')
-args = parser.parse_args()
 
 def ustcmail(sname, passwd, racc, text="mail test by liuboss"):
     # sname : name of sender account, must be ustc mail
@@ -18,7 +11,7 @@ def ustcmail(sname, passwd, racc, text="mail test by liuboss"):
     message = MIMEText(text, 'plain', 'utf-8')
     message['From'] = Header("<"+sacc+">", 'utf-8')
     message['To'] =  Header(racc, 'utf-8')
-    subject = 'Resources Monitor'
+    subject = 'Python SMTP Test'
     message['Subject'] = Header(subject, 'utf-8')
 
     smtpObj = smtplib.SMTP()
@@ -27,4 +20,4 @@ def ustcmail(sname, passwd, racc, text="mail test by liuboss"):
     smtpObj.sendmail(sacc, racc, message.as_string())
 
 if __name__ == "__main__":
-    ustcmail(args.sn,args.pw,args.ra,"Hello")
+    ustcmail("lcb592","XXX","774054270@qq.com","Hello")
