0bc71320214c1cf4db05d3556fd47542d1bb3c1a
liuboss1992 <liuchuanbin1992@gmail.com>
2018-10-26 12:07:42 +0800

    Choose gpu type
    添加了选择gpu型号的功能。
    0:debug
    1:1080ti
    2:v100
    3:titanxp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from mailapp import ustcmail
-from monitor import monitor1080
+from monitor import monitordebug, monitor1080, monitorv100, monitorxp
 import argparse
 
 parser = argparse.ArgumentParser(description='Mail Sender')
@@ -10,19 +10,32 @@ parser.add_argument('--mem', default='8192', type=int, help='Memory Size')
 parser.add_argument('--sn', default='lcb592', type=str, help='name of sender account, must be ustc mail')
 parser.add_argument('--pw', default='123456', type=str, help='password of your account')
 parser.add_argument('--ra', default='774054270@qq.com', type=str, help='address of the receiver')
+parser.add_argument('--type', default='1', type=int, help='GPU Type: 0->debug, 1->1080ti, 2->v100, 3->xp')
 args = parser.parse_args()
 
 def main():
+
     i = 0
     while (1-i):
-        i,text = monitor1080(args.cpu,args.gpu,args.mem)
+        if args.type == 0:
+            i,text = monitordebug(args.cpu,args.gpu,args.mem)
+        elif args.type == 1:
+            i,text = monitor1080(args.cpu,args.gpu,args.mem)
+        elif args.type == 2:
+            i,text = monitorv100(args.cpu,args.gpu,args.mem)
+        elif args.type == 3:
+            i,text = monitorxp(args.cpu,args.gpu,args.mem)
+        else:
+            print("You can only choose 0,1,2,3 as the type of GPU!")
+            break
+
         if i:
             ustcmail(args.sn,args.pw,args.ra,text)
             print("Found!\n")
             print(text)
         else:
-        	print("Not Found Yet!")
-        time.sleep(3)
+            print("Not Found Yet!")
+            time.sleep(3)
 
 if __name__ == "__main__":
     main()
--- monitor.py
+++ monitor.py
@@ -8,6 +8,34 @@ import argparse
 #parser.add_argument('--mem', default='8192', type=int, help='Memory Size')
 #args = parser.parse_args()
 
+def monitordebug(cpu=4,gpu=1,memory=8192):
+    #os.system("wget https://cloud.bitahub.com/resources/gtx1080ti -O gtx1080ti > /dev/null 2>&1")
+    os.system("wget -q https://cloud.bitahub.com/resources/debug -O debug")
+    f = open('./debug', 'r')
+    data = f.read()
+    f.close()
+
+    key = data
+    p1 = r"\n    \"(\S+)\": {\n     "+ \
+          " \"cpu\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }, \n      "+ \
+          "\"gpu\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }, \n      "+ \
+          "\"memory-MB\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }\n    },"
+
+    pattern1 = re.compile(p1,re.DOTALL)
+    matcher1 = re.findall(pattern1,key)
+    flag = 0
+    text = 'Resource Requirement: Debug, CPU ' + str(cpu) + ', GPU ' + str(gpu) + ', Memory ' + str(memory) + '\n'
+    for match in matcher1:
+        #print(match)
+        if ((int(match[1]))>=cpu)and(((int(match[3]))>=gpu))and((int(match[5]))>=memory):
+            #print(match)
+            #print("find!")
+            flag = 1
+            text += 'Node ' + str(match[0])+ ', CPU Left ' + str(match[1]) + ', GPU Left ' + str(match[3]) + ', Memory Left ' + str(match[5]) + '\n'
+    #print(text)
+    return flag, text
+
+
 def monitor1080(cpu=4,gpu=1,memory=8192):
     #os.system("wget https://cloud.bitahub.com/resources/gtx1080ti -O gtx1080ti > /dev/null 2>&1")
     os.system("wget -q https://cloud.bitahub.com/resources/gtx1080ti -O gtx1080ti")
@@ -24,7 +52,7 @@ def monitor1080(cpu=4,gpu=1,memory=8192):
     pattern1 = re.compile(p1,re.DOTALL)
     matcher1 = re.findall(pattern1,key)
     flag = 0
-    text = 'Resource Requirement: CPU ' + str(cpu) + ', GPU ' + str(gpu) + ', Memory ' + str(memory) + '\n'
+    text = 'Resource Requirement: 1080Ti, CPU ' + str(cpu) + ', GPU ' + str(gpu) + ', Memory ' + str(memory) + '\n'
     for match in matcher1:
         #print(match)
         if ((int(match[1]))>=cpu)and(((int(match[3]))>=gpu))and((int(match[5]))>=memory):
@@ -35,12 +63,81 @@ def monitor1080(cpu=4,gpu=1,memory=8192):
     #print(text)
     return flag, text
 
+def monitorv100(cpu=4,gpu=1,memory=8192):
+    #os.system("wget https://cloud.bitahub.com/resources/gtx1080ti -O gtx1080ti > /dev/null 2>&1")
+    os.system("wget -q https://cloud.bitahub.com/resources/teslav100 -O teslav100")
+    f = open('./teslav100', 'r')
+    data = f.read()
+    f.close()
+
+    key = data
+    p1 = r"\n    \"(\S+)\": {\n     "+ \
+          " \"cpu\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }, \n      "+ \
+          "\"gpu\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }, \n      "+ \
+          "\"memory-MB\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }\n    },"
+
+    pattern1 = re.compile(p1,re.DOTALL)
+    matcher1 = re.findall(pattern1,key)
+    flag = 0
+    text = 'Resource Requirement: V100, CPU ' + str(cpu) + ', GPU ' + str(gpu) + ', Memory ' + str(memory) + '\n'
+    for match in matcher1:
+        #print(match)
+        if ((int(match[1]))>=cpu)and(((int(match[3]))>=gpu))and((int(match[5]))>=memory):
+            #print(match)
+            #print("find!")
+            flag = 1
+            text += 'Node ' + str(match[0])+ ', CPU Left ' + str(match[1]) + ', GPU Left ' + str(match[3]) + ', Memory Left ' + str(match[5]) + '\n'
+    #print(text)
+    return flag, text
+
+def monitorxp(cpu=4,gpu=1,memory=8192):
+    #os.system("wget https://cloud.bitahub.com/resources/gtx1080ti -O gtx1080ti > /dev/null 2>&1")
+    os.system("wget -q https://cloud.bitahub.com/resources/titanxp -O titanxp")
+    f = open('./titanxp', 'r')
+    data = f.read()
+    f.close()
+
+    key = data
+    p1 = r"\n    \"(\S+)\": {\n     "+ \
+          " \"cpu\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }, \n      "+ \
+          "\"gpu\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }, \n      "+ \
+          "\"memory-MB\": {\n        \"left\": (\d+), \n        \"total\": (\d+)\n      }\n    },"
+
+    pattern1 = re.compile(p1,re.DOTALL)
+    matcher1 = re.findall(pattern1,key)
+    flag = 0
+    text = 'Resource Requirement: titanxp, CPU ' + str(cpu) + ', GPU ' + str(gpu) + ', Memory ' + str(memory) + '\n'
+    for match in matcher1:
+        #print(match)
+        if ((int(match[1]))>=cpu)and(((int(match[3]))>=gpu))and((int(match[5]))>=memory):
+            #print(match)
+            #print("find!")
+            flag = 1
+            text += 'Node ' + str(match[0])+ ', CPU Left ' + str(match[1]) + ', GPU Left ' + str(match[3]) + ', Memory Left ' + str(match[5]) + '\n'
+    #print(text)
+    return flag, text
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Resources Monitor')
     parser.add_argument('--cpu', default='4', type=int, help='CPU Number')
     parser.add_argument('--gpu', default='1', type=int, help='GPU Number')
     parser.add_argument('--mem', default='8192', type=int, help='Memory Size')
+    parser.add_argument('--type', default='1', type=int, help='GPU Type: 0->debug, 1->1080ti, 2->v100, 3->xp')
     args = parser.parse_args()
 
-    monitor1080(args.cpu,args.gpu,args.mem)
+    if args.type == 0:
+        _,text = monitordebug(args.cpu,args.gpu,args.mem)
+        print(text)
+    elif args.type == 1:
+        _,text = monitor1080(args.cpu,args.gpu,args.mem)
+        print(text)
+    elif args.type == 2:
+        _,text = monitorv100(args.cpu,args.gpu,args.mem)
+        print(text)
+    elif args.type == 3:
+        _,text = monitorxp(args.cpu,args.gpu,args.mem)
+        print(text)
+    else:
+        print("You can only choose 0,1,2,3 as the type of GPU")
